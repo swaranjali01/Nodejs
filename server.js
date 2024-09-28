9739f69a7fea8b71da6de8bcfa394e00710924f5
swaranjali01 <swaragawande26@gmail.com>
2024-09-28 22:41:08 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+function add(a,b){
+    return a+b;
+}
+
+var result =add(2,5);
+console.log(result);
+
+// let fs =require('fs');
+// let os =require('os');
+
+// let user = os.userInfo();
+// console.log(user);
+// console.log(user.username);
+
+// fs.appendFile('greeting.txt','Hi'+user.username + '!\n', ()=>(
+//      console.log('file is created')));
+
+//      console.log(fs);
+     
