968f5cc2fad8e222f6ceba731f12c5355b7e3c05
swaranjali01 <swaragawande26@gmail.com>
2024-10-01 00:51:52 +0530

    commit

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const notes = require('./notes.js');
 console.log('server file is available');
 
-var age = notes.app;
+var age = notes.age;
+
+var result = notes.addNumber(age+18,10);
+
+
 console.log(age);
+console.log('result is now '+result);
+
 
 // function add(a,b){
 //     return a+b;
