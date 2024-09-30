6b2156bd05087c17b5fb5b9da2559b70b7228908
swaranjali01 <swaragawande26@gmail.com>
2024-10-01 01:26:08 +0530

    commit

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const notes = require('./notes.js');
+var_ = require('lodash');//for using uniq to filter
+
 console.log('server file is available');
-
 var age = notes.age;
-
 var result = notes.addNumber(age+18,10);
-
-
 console.log(age);
 console.log('result is now '+result);
+
+var data = ["person",1,2,1,2,'name','age','2'];
+var filter=_.uniq(data);
+console.log(filter);
 
 
 // function add(a,b){
